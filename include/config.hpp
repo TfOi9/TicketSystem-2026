f426a8427486022d13a3c6030c19342064243bf8
TfOi9 <tfoi92024@sjtu.edu.cn>
2026-04-27 20:52:16 +0800

    test: disabled all features except bpt note: this is a temporary update

--- include/config.hpp
+++ include/config.hpp
@@ -11,7 +11,7 @@ typedef int64_t diskpos_t;
 constexpr size_t PAGE_SLOT_COUNT = 200;
 static_assert(PAGE_SLOT_COUNT % 2 == 0, "Slot count must be even!");
 
-constexpr size_t CACHE_CAPACITY = 15;
+constexpr size_t CACHE_CAPACITY = 1600;
 
 typedef int64_t hash_t;
 
